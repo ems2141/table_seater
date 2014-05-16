dc259e604d4b24fde68e2ee07bbd7a6ce755f5e6
Ellie Schneiders <ems2141@gmail.com>
2014-05-16 10:53:23 -0600

    [#71475016] solved using a split, merge algorithm

--- lib/table_seater.rb
+++ lib/table_seater.rb
@@ -6,33 +6,17 @@ class TableSeater
 
   def arrange
     i = 0
-    pairs = {}
-    while i < @people.length-2 do
-      @people.each do |person|
-        person_to_sit_by = @people[i+2]
-        if person_to_sit_by != nil
-          pairs[person] = person_to_sit_by
-        end
-        i += 1
-      end
-      i
-    end
-    pairs
+    first_half = []
+    second_half = []
 
-    results = []
-    pairs.each do |key, value|
-      if !results.include?(key)
-        results << key
-      elsif !results.include?(value)
-        results << value
-      elsif (pairs[value] != nil) && (!results.include?(pairs[value]))
-        results << pairs[value]
+    @people.each do |person|
+      if  i%2 == 0
+        first_half << person
+      else
+        second_half << person
       end
+      i += 1
     end
-    results
-    # for each person/key, make sure that they sit next to their value
-    # check if the value is a key elsewhere in the pairs hash and put them next to their value
-    # move to next pair
-
+    first_half.concat(second_half)
   end
 end
